813bedf6cf8f53332a0aa0d4edb8acd51200e718
BlackMaster201 <jefemaestromenor117@gmail.com>
2025-07-19 00:35:24 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'ygo-pairings-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/LCG.png',
+  '/manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.4.1/papaparse.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.url.includes('Ronda-')) {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return fetch(event.request).then(networkResponse => {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+        }).catch(() => response);
+      })
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
+    );
+  }
+});
